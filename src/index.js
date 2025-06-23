fd3eaa2ab9fbae3853809ba968941cb709e2f536
Monica-G30 <wangui16monica@gmail.com>
2025-06-23 17:02:35 +0300

    Added the AI api intergration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "Baba asanti kwa kunipa uhai",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "2ab48f58932e542cdo049f3057b190t0";
+  let context =
+    "You're an adventerous poem expert and love to write short Swahili poems.Your mission is to generate a four line poem separating each line with <br/>.Make sure to follow the user instructions";
+  let prompt = `User instructions:Generate a Swahili poem about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem");
+  console.log(`prompt.${prompt}`);
+  console.log(`context.${context}`);
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
